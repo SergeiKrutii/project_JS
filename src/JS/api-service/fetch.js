0ce1e05dac431a7da484d7a8bbb5675c718396b8
unknown <sergei.krutii01@gmail.com>
2023-02-05 23:17:49 +0300

    start

--- src/JS/api-service/fetch.js
+++
@@ -1,3 +0,0 @@
-console.log('Это консолько api ^_^')
-console.log('Это кол ^_^')
-console.log('Это кол2 ^_^')
--- src/JS/api-service/markup.js
+++
@@ -1,3 +0,0 @@
-console.log('Это консолько markup ^_^')
-console.log('Это конь')
-console.log('Это конь2')
