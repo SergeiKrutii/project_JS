2bda0be5c6909e0686793b613bbf6d6cc1c7e918
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2023-02-08 00:02:22 +0200

    add markup

--- src/JS/api-service/create-event.js
+++ src/JS/api-service/create-event.js
@@ -1,13 +1,12 @@
 import ApiFetch from "./fetch";
-import { makeMarkup } from "./markup";
+import { eventSettings } from "./markup";
 
 const newClass = new ApiFetch();
 
 
 async function createEvent() {
     const eventData = await newClass.fetchData();
-    const markup = makeMarkup(eventData)
+    const markup = eventSettings(eventData);
     // console.log(eventData)
 }
 
-createEvent();
