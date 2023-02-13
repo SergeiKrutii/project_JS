ebfeeb373e95f4875e09d89cd3b4c31fd9db8e8c
KolyaKhmarjuk <kolyakhmarjuk@gmail.com>
2023-02-13 21:06:21 +0200

    drobmenu

--- src/JS/api-service/refs-api.js
+++ src/JS/api-service/refs-api.js
@@ -2,6 +2,7 @@ const refs = {
   startForm: document.querySelector('.startSearch'),
   cheeseClick: document.querySelector('.click-cheese'),
   countryList: document.querySelector('.country-list'),
+  countryListLi: document.querySelector('.country-list-li'),
 };
 
 export { refs };
