9466d9eb2199cdc71b7dd601404e12a1730a916b
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2023-02-18 00:08:18 +0200

    please work

--- src/JS/api-service/openModal.js
+++ src/JS/api-service/openModal.js
@@ -1,15 +1,33 @@
 import { refs } from './refs-api';
+import ApiFetch from './fetch';
+import { eventById, eventByName } from './countryFilter';
 
 refs.divBlock.addEventListener('click', openModal);
 refs.svgModal.addEventListener('click', closeModal);
+refs.moreBtnModal.addEventListener('click', onLoadMorAuthor);
 
+let artistName = '';
+
+console.log('🚀 ~ out', artistName);
 function openModal(e) {
+  let dataId = '';
+  dataId = e.target.offsetParent.dataset.id;
+
+  artistName = e.target.offsetParent.dataset.artname;
+
   if (
     e.target.className === 'event__card' ||
-    e.target.className === 'event__name'
+    e.target.className === 'event__name' ||
+    e.target.className === 'event__image'
   ) {
     refs.modal.style.display = 'block';
+    eventById(dataId);
   }
+}
+
+function onLoadMorAuthor() {
+  eventByName(artistName);
+  closeModal();
 }
 
 function closeModal() {
@@ -21,4 +39,5 @@ window.onclick = function (e) {
     refs.modal.style.display = 'none';
   }
 };
+
 
